0fe39db768b89a475b69b235f114342eebd58373
balapi <frank.dimeo@broadcom.com>
2024-05-21 07:27:26 -0400

    Update bcmolt_api_code_helper.c
    fixed license header

--- api/bcmolt_api_code_helper.c
+++ api/bcmolt_api_code_helper.c
@@ -1,30 +1,23 @@
 /*
-  <:copyright-BRCM:2016:DUAL/GPL:standard
-
-  Broadcom Proprietary and Confidential.(c) 2016 Broadcom
-  All Rights Reserved
-
-  Unless you and Broadcom execute a separate written software license
-  agreement governing use of this software, this software is licensed
-  to you under the terms of the GNU General Public License version 2
-  (the "GPL"), available at http://www.broadcom.com/licenses/GPLv2.php,
-  with the following added to such license:
-
-  As a special exception, the copyright holders of this software give
-  you permission to link this software with independent modules, and
-  to copy and distribute the resulting executable under terms of your
-  choice, provided that you also meet, for each linked independent
-  module, the terms and conditions of the license of that module.
-  An independent module is a module which is not derived from this
-  software.  The special exception does not apply to any modifications
-  of the software.
-
-  Not withstanding the above, under no circumstances may you combine
-  this software in any way with any other Broadcom software provided
-  under a license other than the GPL, without Broadcom's express prior
-  written consent.
-
-  :>
+<:copyright-BRCM:2016-2024:Apache:standard
+ 
+ Copyright (c) 2016-2024 Broadcom. All Rights Reserved
+ 
+ The term “Broadcom” refers to Broadcom Inc. and/or its subsidiaries
+ 
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+ 
+     http://www.apache.org/licenses/LICENSE-2.0
+ 
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ 
+ *  :>
 */
 
 #include <bcmos_system.h>
